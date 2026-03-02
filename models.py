704c2e3f11083bf1072fb6db3815fe566e4abaf7
danieltbailey47-droid <danieltbailey47@gmail.com>
2026-03-02 16:43:41 -0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Note(models.Model):
+
+    title = models.CharField(max_length=500)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    author = models.ForeignKey(
+        "Author", on_delete=models.CASCADE, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+
+    name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
